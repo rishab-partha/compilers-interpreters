8475a69f29bec990b080a4a39f6bbcce40ea3e76
Rishab Parthasarathy <rishabparthasarathy@gmail.com>
2020-02-06 14:33:17 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main
      */
     public static void main(String[] args) throws IOException
     {
-        BufferedReader in = new BufferedReader(new FileReader("Scanner//ScannerTestAdvanced.txt"));
+        BufferedReader in = new BufferedReader(new FileReader("Scanner//ScannerTest.txt"));
         Scanner sc = new Scanner(in);
         while (sc.hasNext())
         {
